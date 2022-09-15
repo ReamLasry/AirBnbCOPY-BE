c0403493edaaaa759c51670ed2cef2a28ac8e6c4
Ream lasry <ream794613@gmail.com>
2022-09-16 00:56:53 +0200

    Update 1

---
+++ api/stay/stay.routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {requireAuth, requireAdmin} = require('../../middlewares/requireAuth.middleware')
+const {log} = require('../../middlewares/logger.middleware')
+const {getStays, deleteStay, addStay,getStayById,updateStay} = require('./stay.controller')
+const router = express.Router()
+
+// middleware that is specific to this router
+// router.use(requireAuth)
+
+router.get('/', log,  getStays)
+router.get('/:stayId', log, getStayById)
+router.post('/', log, addStay)
+router.delete('/:stayId',  requireAuth, deleteStay)
+router.put('/:toyId', log, requireAuth, updateStay)
+
+
+module.exports = router
